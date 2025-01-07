0784238350a6ea9eb09e777cdc42864b4c65e4d7
shashankreddy99TUS <k.shashankreddy599@gmail.com>
2025-01-07 13:50:33 +0530

    shell script

---
+++ 07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("pushpa","rrr","devara")
+#index starts from 0,sixe is 3
+
+echo "first movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
+
+echo "All movies are: $(MOVIES[@])" 
