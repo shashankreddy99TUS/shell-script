b183e6045d1b25b727c8dde54b527ded1763a58e
shashankreddy99TUS <k.shashankreddy599@gmail.com>
2025-01-08 14:43:00 +0530

    shell script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,22 +8,47 @@ then
    exit i
    fi
 
+  if[$? -ne 0]
    dnf install mysql -y
-
    if[$? -ne 0]
    then
    echo "Installing MySQL....FAILURE"
    exit 1
    echo "Installing Mysql....SUCCESS"
    fi
+   else
+   echo "MYSQL is already .... INSTALLED"
 
+
+
+   #if[$? -ne 0]
+   #then
+   #echo "Installing MySQL....FAILURE"
+   #exit 1
+   #echo "Installing Mysql....SUCCESS"
+   #fi
+
+dnf list installed git
+
+if[$? -ne 0]
+then
    dnf install git -y
 
-   if[$? -ne 0]
-   then
-   echo "Installing Git....FAILURE"
+if[$? -ne 0]
+then
+echo "Installing Git....FAILURE"
    exit 1
    echo "Installing Git....SUCCESS"
    fi
+  else
+   echo "GIT is already .... INSTALLED"
+
+
+   #if[$? -ne 0]
+   #then
+   #echo "Installing Git....FAILURE"
+   #exit 1
+   #echo "Installing Git....SUCCESS"
+   #fi
 
 
