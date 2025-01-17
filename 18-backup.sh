5c4bc4f600730e8929267e5d34ced65cb8bcb639
shashankreddy99TUS <k.shashankreddy599@gmail.com>
2025-01-17 14:44:39 +0530

    Shell script

--- 18-backup.sh
+++ 18-backup.sh
@@ -54,3 +54,10 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
 echo "Files are: $FILES"
+
+if[-n "$FILES" ]
+then 
+   echo "Files are: $FILES"
+else 
+    echo "No files in zip"
+fi        
