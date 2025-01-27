ca1e4b944f0e1e7285780c71155594e6a6bae8be
shashankreddy99TUS <k.shashankreddy599@gmail.com>
2025-01-27 11:38:22 +0530

    session-11

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -3,10 +3,16 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 #real projects will monitor for 70
+MSG=""
 
 while read -r line
 do
-  USAGE =$(echo $line | awk -F " " '{print $6F}')
+  USAGE=$(echo $line | awk -F " " '{print $6F}' |cut -d "%" -fi)
   PARTITION=$(echo $line | awk -F " " '{print $NF}')
-  echo "Partition: $PARTITION , Usage: $USAGE"
-done <<< $DISK_USAGE 
+  #echo "Partition: $PARTITION , Usage: $USAGE"
+  if[ $USAGE -ge $DISK_THRESHOLD ]
+  then 
+     MSG="High Desk usage on partition: $PARTITION Usage is: $USAGE"
+    fi 
+
+done <<< $DISK_USAGE  
